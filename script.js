9e7c5d9e01da8217b29c753b69a890c96b62f36c
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-23 16:55:47 +0100

    application complited

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const  messageContainer = document.querySelector('#send-container')
 const  messageForm = document.querySelector('#send-container')
 const  messageInput = document.querySelector('#message-input')
 
+const name = prompt("What is your name?")
+appendMessage('You joined')
+socket.emit('new-user', name)
+
 socket.on('chat-message', data => {
-    appendMessage(data)
+    appendMessage(`${data.name} ${data.message}`)
+})
+
+socket.on('user-connected', data => {
+    appendMessage(`${data} connected`)
+})
+
+socket.on('user-disconnected', data => {
+    appendMessage(`${data} disconnected`)
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    appendMessage(`You ${message}`)
     socket.emit('send-message', message)
     messageInput.value = ''
 })
--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const io = require('socket.io')(3000,{cors: {
     origin: "http://127.0.0.1:5500"
 }})
 
+const users = {}
+
 io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
     socket.on("send-message", message => {
-        socket.broadcast.emit("chat-message", message)
+        socket.broadcast.emit("chat-message", { message, name: users[socket.id]})
     })
 })
