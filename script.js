2b32b1e417d07be420d53beb1b3524813e53a310
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-23 16:28:13 +0100

    append messages to the client completed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const socket = io('http://localhost:3000')
+const  messageContainer = document.querySelector('#send-container')
 const  messageForm = document.querySelector('#send-container')
 const  messageInput = document.querySelector('#message-input')
 
 socket.on('chat-message', data => {
-    console.log(data)
+    appendMessage(data)
 })
 
 messageForm.addEventListener('submit', e => {
@@ -12,3 +13,9 @@ messageForm.addEventListener('submit', e => {
     socket.emit('send-message', message)
     messageInput.value = ''
 })
+
+function appendMessage(message) {
+    const messageElement  = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
