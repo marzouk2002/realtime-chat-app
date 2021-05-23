62cf580ac9f6d36a4823078723ce4d3b66ef65ee
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-23 16:01:45 +0100

    script created

---
+++ server.js
@@ -0,0 +1,8 @@
+const io = require('socket.io')(3000,{cors: {
+    origin: "http://127.0.0.1:5500"
+}})
+
+io.on('connection', socket => {
+    console.log("new user")
+    socket.emit("chat-message", 'Hello World')
+})
